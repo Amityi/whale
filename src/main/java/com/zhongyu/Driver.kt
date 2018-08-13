80e12d40afbcd3ae798a718ebc8266dd82a45adf
Unknown <zhongyu.vip@gmail.com>
2018-08-13 16:30:26 +0800

    修改手机系统的语言

--- src/main/java/com/zhongyu/Driver.kt
+++ src/main/java/com/zhongyu/Driver.kt
@@ -1,11 +1,13 @@
 package com.zhongyu
 
+import com.zhongyu.utils.AddressUtil
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
 import io.appium.java_client.ios.IOSDriver
 import io.appium.java_client.remote.AndroidMobileCapabilityType
 import io.appium.java_client.remote.MobileCapabilityType
+import org.apache.commons.lang3.StringUtils
 import org.openqa.selenium.Platform
 import org.openqa.selenium.remote.DesiredCapabilities
 import java.io.File
@@ -22,11 +24,28 @@ class Driver {
 
     fun driver(): AppiumDriver<*>? {
         val capabilities = DesiredCapabilities()
+        var site = System.getProperty("site")
         val platform = System.getProperty("platform")
         val address = URL("http://127.0.0.1:4723/wd/hub")
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "none")
         capabilities.setCapability(MobileCapabilityType.NO_RESET, false)
-        driver = if (platform == "android") {
+        if (StringUtils.isNotBlank(site) && StringUtils.isNotEmpty(site)) {
+            var country = AddressUtil.instance!!.exists(site)
+            if (country) {
+                capabilities.setCapability(MobileCapabilityType.LANGUAGE, AddressUtil.instance!!.getInfo(site, "language"))
+                capabilities.setCapability(MobileCapabilityType.LOCALE, AddressUtil.instance!!.getInfo(site, "locale"))
+            } else {
+                throw RuntimeException("站点配置未找到!")
+            }
+        } else {
+            capabilities.setCapability(MobileCapabilityType.LANGUAGE, "zh")
+            capabilities.setCapability(MobileCapabilityType.LOCALE, "CN")
+        }
+        driver = if (platform == "iOS") {
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.IOS)
+            capabilities.setCapability(MobileCapabilityType.APP, getApp("zzkko-release.app"))
+            IOSDriver(address, capabilities)
+        } else {
             capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID)
             capabilities.setCapability(MobileCapabilityType.APP, getApp("zzkko-google-release.apk"))
             capabilities.setCapability(AndroidMobileCapabilityType.UNICODE_KEYBOARD, false)
@@ -34,10 +53,6 @@ class Driver {
             capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.zzkko")
             capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.zzkko.bussiness.login.ui.WelcomeActivity")
             AndroidDriver(address, capabilities)
-        } else {
-            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.IOS)
-            capabilities.setCapability(MobileCapabilityType.APP, getApp("zzkko-release.app"))
-            IOSDriver(address, capabilities)
         }
         driver!!.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS)
         return driver
@@ -73,6 +88,7 @@ class Driver {
 
         @JvmStatic
         fun main(args: Array<String>) {
+            System.setProperty("site", "hk")
             val driver = Driver.instance!!.driver()
             driver!!.findElementById("tv_skip").click()
             driver.quit()
