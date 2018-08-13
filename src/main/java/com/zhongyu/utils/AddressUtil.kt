6510471c6a998b1f8e81ff85bf0f0cbb405d259b
ZhongYuOwO <zhongyu.vip@gmail.com>
2018-08-13 14:44:26 +0800

    add addess util

---
+++ src/main/java/com/zhongyu/utils/AddressUtil.kt
@@ -0,0 +1,82 @@
+package com.zhongyu.utils
+
+import com.esotericsoftware.yamlbeans.YamlConfig
+import com.esotericsoftware.yamlbeans.YamlException
+import com.esotericsoftware.yamlbeans.YamlReader
+
+import java.io.FileNotFoundException
+import java.io.FileReader
+
+/**
+ * @author ZhongYu
+ */
+class AddressUtil {
+
+    fun getInfo(root: String, key: String): String {
+        val yml = yaml
+        try {
+            val maps = yml!![root] as Map<*, *>
+            return maps[key].toString()
+        } catch (e: NullPointerException) {
+            throw RuntimeException("元素未找到!")
+        }
+
+    }
+
+    fun getAddress(root: String, key: String): String {
+        val yml = yaml
+        try {
+            val maps = yml!![root] as Map<*, *>
+            val address = maps["address"] as Map<String, Any>
+            return address[key].toString()
+        } catch (e: NullPointerException) {
+            throw RuntimeException("元素未找到!")
+        }
+
+    }
+
+    companion object {
+
+        @Volatile
+        private var addressUtil: AddressUtil? = null
+
+        private val instance: AddressUtil?
+            get() {
+                if (addressUtil == null) {
+                    synchronized(AddressUtil::class.java) {
+                        if (addressUtil == null) {
+                            addressUtil = AddressUtil()
+                        }
+                    }
+                }
+                return addressUtil
+            }
+
+        private val yaml: Map<String, Any>?
+            get() {
+                val config = YamlConfig()
+                config.setAllowDuplicates(false)
+                val reader: YamlReader
+                var `object`: Any? = null
+                try {
+                    reader = YamlReader(FileReader("src/main/resources/address.yml"), config)
+                    `object` = reader.read()
+                } catch (e: FileNotFoundException) {
+                    throw RuntimeException("address.yml文件未找到")
+                } catch (e: YamlException) {
+                    e.printStackTrace()
+                }
+
+                return `object` as Map<String, Any>?
+            }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val info = AddressUtil.instance!!.getInfo("hk", "name")
+            val address = AddressUtil.instance!!.getAddress("hk", "address_1")
+            println(info)
+            println(address)
+        }
+    }
+
+}
