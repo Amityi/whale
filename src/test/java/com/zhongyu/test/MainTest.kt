4aabe151e08ff63223cbd1172a390e9f63f63d0c
Unknown <zhongyu.vip@gmail.com>
2018-08-14 13:49:07 +0800

    添加主要测试类
    用于初始化应用程序

---
+++ src/test/java/com/zhongyu/test/MainTest.kt
@@ -0,0 +1,37 @@
+package com.zhongyu.test
+
+import com.zhongyu.Driver
+import com.zhongyu.business.HomeBusiness
+import io.appium.java_client.MobileDriver
+import org.testng.Reporter
+import org.testng.annotations.AfterClass
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.Test
+
+/**
+ * @author ZhongYu
+ */
+class MainTest {
+
+    private var driver: MobileDriver<*>? = null
+    private var homeBusiness: HomeBusiness? = null
+
+    @BeforeClass
+    @Throws(Exception::class)
+    fun setUp() {
+        driver = Driver.instance!!.driver()
+        homeBusiness = driver?.let { HomeBusiness(it) }
+    }
+
+    @Test(priority = 0)
+    fun switchCountry() {
+        Reporter.log("切换站点")
+    }
+
+    @AfterClass
+    @Throws(Exception::class)
+    fun tearDown() {
+        driver!!.quit()
+    }
+
+}
