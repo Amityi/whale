a881006a12b8380464a92a8e067a63b1074f8642
Unknown <zhongyu.vip@gmail.com>
2018-08-14 22:12:30 +0800

    update test suite

--- src/test/java/com/zhongyu/test/MainTest.kt
+++ src/test/java/com/zhongyu/test/MainTest.kt
@@ -4,9 +4,7 @@ import com.zhongyu.Driver
 import com.zhongyu.business.HomeBusiness
 import io.appium.java_client.MobileDriver
 import org.testng.Reporter
-import org.testng.annotations.AfterClass
-import org.testng.annotations.BeforeClass
-import org.testng.annotations.Test
+import org.testng.annotations.*
 
 /**
  * @author ZhongYu
@@ -16,11 +14,18 @@ class MainTest {
     private var driver: MobileDriver<*>? = null
     private var homeBusiness: HomeBusiness? = null
 
-    @BeforeClass
+    @BeforeMethod
     @Throws(Exception::class)
     fun setUp() {
         driver = Driver.instance!!.driver()
         homeBusiness = driver?.let { HomeBusiness(it) }
+    }
+
+    @Test(priority = 2, description = "执行登录测试")
+    fun loginSuccess() {
+        Reporter.log("登录测试")
+        homeBusiness!!.toLogin("zhongyu.vip@gmail.com", "Shein12345")
+        Thread.sleep(3000)
     }
 
     @Test(priority = 0)
@@ -29,7 +34,7 @@ class MainTest {
         homeBusiness!!.switch()
     }
 
-    @AfterClass
+    @AfterMethod
     @Throws(Exception::class)
     fun tearDown() {
         driver!!.quit()
