eb0891d02decc2e432ec811b44882344772b2e04
ZhongYuOwO <zhongyu.vip@gmail.com>
2018-08-17 14:51:53 +0800

    根据站点使用的语言区分

--- src/test/java/com/zhongyu/business/ShopCartBusiness.kt
+++ src/test/java/com/zhongyu/business/ShopCartBusiness.kt
@@ -14,17 +14,28 @@ class ShopCartBusiness(driver: MobileDriver<*>) {
     private val categoryHandle: CategoryHandle = CategoryHandle(driver)
     private val detailHandle: DetailHandle = DetailHandle(driver)
 
-    private fun init(){
-        homeHandle.clickSkip()
-        homeHandle.clickCategory()
+    /**
+     * 根据站点使用相对应的语言
+     */
+    private fun judge(){
+        val site = homeHandle.getCountryText()
+        if (site == "HK" || site == "TW") {
+            categoryHandle.clickTopsCn()
+            categoryHandle.clickTShirtCn()
+        } else if (site == "ES" || site == "MX") {
+            categoryHandle.clickTopsEn()
+            categoryHandle.clickTShirtEs()
+        } else {
+            categoryHandle.clickTopsEn()
+            categoryHandle.clickTShirtEn()
+        }
     }
 
     fun addToCart(): String {
-        init()
-        var count = homeHandle.getBagCount()
-        //TODO: 需要判断当前使用的语言
-        categoryHandle.clickTopsEn()
-        categoryHandle.clickTShirtEs()
+        homeHandle.clickSkip()
+        homeHandle.clickCategory()
+        val count = homeHandle.getBagCount()
+        judge()
         categoryHandle.clickThree()
         categoryHandle.clickItem()
 //        detailHandle.clickComment()
@@ -33,9 +44,10 @@ class ShopCartBusiness(driver: MobileDriver<*>) {
         return count
     }
 
-    fun buy(){
-        init()
-        categoryHandle.clickTopsEn()
+    fun buy() {
+        homeHandle.clickSkip()
+        homeHandle.clickCategory()
+        judge()
     }
 
     fun count(): String = homeHandle.getBagCount()
